b5cdffcacc9186f9fe270b26daa32e7636a389bf
Dan Olner <danolner@gmail.com>
2023-11-07 17:29:19 +0000

    SICSOC update

--- explore_code/sic_soc_explore.R
+++ explore_code/sic_soc_explore.R
@@ -195,17 +195,25 @@ z <- z %>%
 z <- z %>% 
   mutate(OBS_VALUE_REGIONALPERCENT = (OBS_VALUE/ALL_IN_EMPLOYMENT_16PLUS)*100)
 
+saveRDS(z, 'data/sicsoc.rds')
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # WHAT'S THE MISSING / ERROR BAR RATE LIKE? TEST WITH SY----
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+unique(z$GEOGRAPHY_NAME)
 
 sy <- z %>% filter(GEOGRAPHY_NAME == 'South Yorkshire')
 sy <- z %>% filter(GEOGRAPHY_NAME %in% c('South Yorkshire','Greater Manchester'))
 sy <- z %>% filter(grepl(pattern = 'South Yorkshire|Manchester|Merseyside|West York', x = GEOGRAPHY_NAME, ignore.case = T))
 sy <- z %>% filter(grepl(pattern = 'South Yorkshire|Manchester|West York', x = GEOGRAPHY_NAME, ignore.case = T))
 sy <- z %>% filter(grepl(pattern = 'South Yorkshire|West Mid', x = GEOGRAPHY_NAME, ignore.case = T))
+sy <- z %>% filter(grepl(pattern = 'South Yorkshire|West Yorkshire', x = GEOGRAPHY_NAME, ignore.case = T))
 sy <- z %>% filter(grepl(pattern = 'South Yorkshire|Merseyside', x = GEOGRAPHY_NAME, ignore.case = T))
+sy <- z %>% filter(grepl(pattern = 'South Yorkshire|Leicester', x = GEOGRAPHY_NAME, ignore.case = T))
+sy <- z %>% filter(grepl(pattern = 'South Yorkshire|East Yorks', x = GEOGRAPHY_NAME, ignore.case = T))
+sy <- z %>% filter(grepl(pattern = 'South Yorkshire|Surrey, East and West Sussex', x = GEOGRAPHY_NAME, ignore.case = T))
+sy <- z %>% filter(grepl(pattern = 'South Yorkshire|Inner London - East', x = GEOGRAPHY_NAME, ignore.case = T))
 
 
 #Eyeballing the OBS_STATUS code, we have:
@@ -267,9 +275,13 @@ sy.ww <- sy.w %>%
     values_fn = mean
   ) %>% 
   mutate(CIs_overlap = ifelse(
-    (`min_ci_Greater Manchester` <= `max_ci_South Yorkshire` & `max_ci_Greater Manchester` <= `min_ci_South Yorkshire`) |
-      (`min_ci_South Yorkshire` <= `max_ci_Greater Manchester` & `max_ci_South Yorkshire` <= `min_ci_Greater Manchester`)  , 
-    T,F))
+    (.[,3] <= .[,6] & .[,5] <= .[,4]) |
+      (.[,4] <= .[,5] & .[,6] <= .[,3])  , 
+    F,T) %>% c(.))#Make generic
+  # mutate(CIs_overlap = ifelse(
+  #   (`min_ci_Greater Manchester` <= `max_ci_South Yorkshire` & `max_ci_Greater Manchester` <= `min_ci_South Yorkshire`) |
+  #     (`min_ci_South Yorkshire` <= `max_ci_Greater Manchester` & `max_ci_South Yorkshire` <= `min_ci_Greater Manchester`)  , 
+  #   F,T))
 
 
 #Merge those back in
@@ -296,15 +308,188 @@ ggplot(
   ylab("") +
   theme(legend.title=element_blank(), plot.title = element_text(face = 'bold')) +
   ggtitle("Occupation (SOC2020) vs sector (SIC2007), SY and GM") +
-  scale_color_manual(values = c('grey','black')) +
-  scale_size_manual(values = c(0.5,1)) +
+  scale_color_manual(values = c('black','grey')) +
+  scale_size_manual(values = c(1,0.5)) +
   guides(colour = F, size = F)
 
-ggsave('local/localimages/sicsoc_GM_SY.png', width = 14, height = 10)
+# ggsave('local/localimages/sicsoc_GM_SY.png', width = 14, height = 10)
 
 
 
 
+
+
+#Sooo can we get all that data to see where skills differ everywhere for SY significantly?
+get_all_places_sicsocs <- function(geography_name,comparator_name){
+  
+  sy <- z %>% filter(GEOGRAPHY_NAME %in% c(comparator_name,geography_name))
+  
+  sy.w <- sy %>% 
+    select(-OBS_VALUE) %>% 
+    pivot_wider(values_from = OBS_VALUE_REGIONALPERCENT, names_from = MEASURES_NAME) %>% 
+    mutate(
+      min_ci = Value - Confidence,
+      max_ci = Value + Confidence
+    )
+  
+  #Mark pairs where CIs do not overlap
+  #Rather than messing around with figuring out lags
+  #make a wider version and do differences there
+  
+  #https://stackoverflow.com/a/3269471
+  #If (StartA <= EndB) and (EndA >= StartB) 
+  sy.ww <- sy.w %>%
+    select(GEOGRAPHY_NAME,SIC2007,SOC2020,min_ci,max_ci) %>% 
+    pivot_wider(
+      names_from = GEOGRAPHY_NAME, values_from = c(min_ci,max_ci),
+      values_fn = mean
+    ) %>% 
+    mutate(CIs_overlap = ifelse(
+      (.[,3] <= .[,6] & .[,5] <= .[,4]) |
+        (.[,4] <= .[,5] & .[,6] <= .[,3])  , 
+      F,T))#Make generic
+  # mutate(CIs_overlap = ifelse(
+  #   (`min_ci_Greater Manchester` <= `max_ci_South Yorkshire` & `max_ci_Greater Manchester` <= `min_ci_South Yorkshire`) |
+  #     (`min_ci_South Yorkshire` <= `max_ci_Greater Manchester` & `max_ci_South Yorkshire` <= `min_ci_Greater Manchester`)  , 
+  #   F,T))
+  
+  
+  #Merge those back in
+  #Applies to both places so can merge in on these
+  
+  #Find difference between actual value, then display if sig (which won't show effect size properly, but...)
+  sy.w %>% 
+    left_join(
+      sy.ww,
+      by = c('SIC2007','SOC2020')
+    ) %>% 
+    group_by(SIC2007,SOC2020) %>% 
+    mutate(
+      valdiff = lag(Value) - Value
+    ) %>% 
+    filter(GEOGRAPHY_NAME == geography_name) %>% 
+    select(GEOGRAPHY_NAME,SOC2020,SIC2007,CIs_overlap,valdiff)
+  
+}
+  
+  
+# chk <- get_all_places_sicsocs('West Yorkshire')
+# table(chk$valdiff)
+
+
+#OK, let's see...
+comparator = 'South Yorkshire'
+comparator = 'Greater Manchester'
+comparator = 'West Yorkshire'
+
+allz <- purrr::map(
+  .f = get_all_places_sicsocs, 
+  .x = unique(z$GEOGRAPHY_NAME[z$GEOGRAPHY_NAME!=comparator]),
+  comparator_name = comparator
+  ) %>% bind_rows
+
+
+# allz <- purrr::map(.f = get_all_places_sicsocs, .x = unique(z$GEOGRAPHY_NAME[z$GEOGRAPHY_NAME!='South Yorkshire'])) %>% bind_rows
+
+allz <- allz %>% 
+  unite(sicsoc, c('SOC2020','SIC2007'), sep = ' || ', remove = F) %>% 
+  mutate(
+    valdiff = ifelse(!CIs_overlap, valdiff, NA)
+  ) 
+  
+#heatmap
+
+#Not quite! 
+# zerocutoff = mean(allz$valdiff <= 0, na.rm = T)
+# ecdf(allz$valdiff)(0)
+
+#This?
+# zerocutoff <- allz$valdiff[!is.na(allz$valdiff)]
+# zerocutoff <- zerocutoff[order(zerocutoff)]
+# max(which(zerocutoff < 0))
+# max(which(zerocutoff < 0))/length(zerocutoff)#Goddam, same number!! Somewhat predictably
+
+#Is it based on vector length, does scaling by total length work? Very much no.
+#cutoff <- max(which(zerocutoff < 0))/length(allz$valdiff)
+
+#It's not just the inverse is it? Nope
+# zerocutoff = 1 - zerocutoff
+
+# zerocutoff = 0.66
+# zerocutoff = allz$valdiff[order(allz$valdiff)]
+
+
+#Actually, it's just the middle position on this scale, not the quantile...?
+#turn into scale with zero in
+#https://www.statology.org/r-scale-between-0-and-1/
+#Tick!
+valz <- c(range(allz$valdiff[allz$SIC2007=='Total Services'], na.rm = T), 0)
+scale_values <- function(x){(x-min(x))/(max(x)-min(x))}
+scaled <- scale_values(valz)
+zerocutoff <- scaled[3]
+
+
+ggplot(allz %>% filter(SIC2007=='Total Services'), aes(x = substr(GEOGRAPHY_NAME,0,20), y = sicsoc, fill= valdiff)) + 
+  geom_tile() +
+  scale_fill_gradientn(
+    colours = c("red", "white", "darkgreen"),
+    values = c(0, zerocutoff, 1)#https://stackoverflow.com/a/58725778/5023561
+  ) +
+  theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=0))
+
+
+
+valz <- c(range(allz$valdiff[allz$SIC2007!='Total Services'], na.rm = T), 0)
+scale_values <- function(x){(x-min(x))/(max(x)-min(x))}
+scaled <- scale_values(valz)
+zerocutoff <- scaled[3]
+
+
+
+#E63946 - A bright red tone
+#F1FAEE - A very light (almost white) pastel green
+#A8DADC - A soft, muted teal
+#457B9D - A desaturated dark blue
+#F4A261 - A sandy brown
+#2A9D8F - A medium sea green
+#6C757D - A neutral dark gray
+
+#FF5733 - A vibrant orange-red
+#CDDC39 - A bright lime green
+#00BCD4 - A bright cyan
+#9C27B0 - A deep purple
+#3F51B5 - A rich indigo
+#E91E63 - A strong pink
+#009688 - A teal
+#FFEB3B - A vivid yellow
+#795548 - A brown
+#607D8B - A cool blue-gray
+
+b <- c(
+  rep('#FF5733',9),
+  rep('#CDDC39',9),
+  rep('#00BCD4',9),
+  rep('#9C27B0',9),
+  rep('#3F51B5',9),
+  rep('#E91E63',9),
+  rep('#009688',9),
+  rep('#FFEB3B',9),
+  rep('#607D8B',9)
+)
+
+ggplot(allz %>% filter(SIC2007!='Total Services'), aes(x = substr(GEOGRAPHY_NAME,0,20), y = sicsoc, fill= valdiff)) + 
+  geom_tile() +
+  scale_fill_gradientn(
+    colours = c("red", "white", "darkgreen"),
+    values = c(0, zerocutoff, 1)#https://stackoverflow.com/a/58725778/5023561
+  ) +
+  theme(axis.text.x = element_text(angle = 270, vjust = 0.5, hjust=0)) +
+  ggtitle(comparator) +
+  theme(
+    plot.title = element_text(face = 'bold'),
+    axis.text.y = element_text(colour = b)
+    ) 
+   
 
 
 

b8ae6aa0602514767c8dfb2c7b59ec9911f080a9
Dan Olner <d.olner@sheffield.ac.uk>
2024-12-17 14:09:14 +0000

    Beatty Fothergill digging

---
+++ explore_code/beatty_fothergill_update.R
@@ -0,0 +1,16 @@
+#Beatty fothergill update to most recent data that matches
+#Cf. https://www.shu.ac.uk/centre-regional-economic-social-research/publications/local-productivity-the-real-differences-across-uk-cities-and-regions
+
+#But we want to do for ITL2s.
+library(tidyverse)
+library(zoo)
+library(sf)
+library(tmap)
+library(plotly)
+library(nomisr)
+library(pryr)
+
+
+
+#RAW REGIONAL GVA DATA----
+
